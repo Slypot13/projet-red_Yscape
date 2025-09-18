044bdc4a64c48fd2ce7d95e3340a088e1e6d182d
Slypot <sylvain.pottier@ynov.com>
2025-09-18 09:38:09 +0200

    YSCAPE

--- src/barredevie.go
+++ src/barredevie.go
@@ -1,4 +1,5 @@
-package main 
+package main
+
 // Barre de vie à afficher dans le terminal
 func DisplayHPBar(current, max int, width int) string {
 	percentage := float64(current) / float64(max)
@@ -15,26 +16,3 @@ func DisplayHPBar(current, max int, width int) string {
 
 	return bar
 }
-
-
-func CharacterTurn(monstre *Monster, player *Character, tour *int, monsterSkipped *bool) {
-	fmt.Println("\n--- Ton tour ---")
-	fmt.Println("1 - Attaquer")
-	fmt.Println("2 - Inventaire")
-	fmt.Println("3 - Utiliser Asics (si équipé) [bloque l'ennemi 1 tour]")
-	fmt.Print("Choix : ")
-	var choix int
-	fmt.Scan(&choix)
-	switch choix {
-	case 1:
-		damage := player.Attaque
-		fmt.Printf("%s utilise Attaque basique et inflige %d dégâts !\n", player.Nom, damage)
-		monstre.Pv -= damage
-		if monstre.Pv < 0 {
-			monstre.Pv = 0
-		}
-
-		// Affichage barre de vie monstre
-		bar := DisplayHPBar(monstre.Pv, monstre.PvMax, 20)
-		fmt.Printf("%s PV : [%s] %d/%d\n", monstre.Nom, bar, monstre.Pv, monstre.PvMax)
-	}
